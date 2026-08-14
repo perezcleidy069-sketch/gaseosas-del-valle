899856289eea4d7e55d01184343b787f74747f7a
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-13 21:11:29 -0600

    feat(dql): Consultar de prueba.

--- dql/consultas.sql
+++ dql/consultas.sql
@@ -21,3 +21,9 @@ SELECT id, fecha, id_cliente, id_sede
     WHERE fecha BETWEEN '2026-07-16 14:55:00'  AND '2026-07-25 09:51:00'
     AND id_sede= 3
     ORDER BY fecha DESC;
+
+    -- Prueba sin especifica el id.
+SELECT id, fecha, id_cliente, id_sede
+	FROM pedidos
+    WHERE fecha BETWEEN '2026-07-16 14:55:00'  AND '2026-07-25 09:51:00'
+    ORDER BY fecha DESC;
