fcd612b2ef2c99f237bc85a49e84cb2b55c355fb
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-13 21:13:09 -0600

    feat(dql): Consultar listar los productos mas vendidos

--- dql/consultas.sql
+++ dql/consultas.sql
@@ -27,3 +27,24 @@ SELECT id, fecha, id_cliente, id_sede
 	FROM pedidos
     WHERE fecha BETWEEN '2026-07-16 14:55:00'  AND '2026-07-25 09:51:00'
     ORDER BY fecha DESC;
+
+    -- 3. Listar los productos más vendidos (con JOIN y GROUP BY).
+SELECT P.id, P.nombre,
+	SUM(C.cantidad) AS total_unidades_vendidas
+FROM productos P 
+JOIN detalle_pedidos C ON P.id = C.id_producto
+GROUP BY P.id, P.nombre
+ORDER BY total_unidades_vendidas DESC
+LIMIT 4;
+-- 3.1 Listar los productos más vendidos (con JOIN y GROUP BY). Pero de una fecha especifica
+SELECT P.id, P.nombre,
+	'Julio 2026' AS periodo,
+	SUM(C.cantidad) AS total_unidades_vendidas
+FROM productos P 
+JOIN detalle_pedidos C ON P.id = C.id_producto
+JOIN pedidos E ON C.id_pedido = E.id
+WHERE MONTH(E.fecha)= 7 AND YEAR(E.fecha)= 2026
+GROUP BY P.id, P.nombre
+ORDER BY total_unidades_vendidas DESC
+LIMIT 3;
+    
