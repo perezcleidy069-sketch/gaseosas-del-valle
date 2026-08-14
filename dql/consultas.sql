b6cb36f5d61f7ef04bf70995887b47c0500520d6
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-08-13 21:10:47 -0600

    feat(dql): Consultar los pedidos realizados entre dos fechas (BETWEEN).

--- dql/consultas.sql
+++ dql/consultas.sql
@@ -14,3 +14,10 @@ SELECT id, nombre, stock_minimo, stock_actual
 	FROM productos
     Where stock_actual< stock_minimo
     ORDER BY stock_actual;
+
+    -- 2.Consultar los pedidos realizados entre dos fechas (BETWEEN).
+SELECT id, fecha, id_cliente, id_sede
+	FROM pedidos
+    WHERE fecha BETWEEN '2026-07-16 14:55:00'  AND '2026-07-25 09:51:00'
+    AND id_sede= 3
+    ORDER BY fecha DESC;
